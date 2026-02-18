7023b4c1b55dc940bbf660de011edc6940d97acd
Sebastian Mygind <sebmygind@gmail.com>
2026-02-18 10:00:03 +0100

    Add line and circle example

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
@@ -26,7 +28,11 @@ int main() {
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, DARKGRAY);
+
+        DrawLineEx(Vector2 {20, 40 }, Vector2 {400, 30}, 7., DARKBLUE);
+
+        DrawCircle(300, 300, 30, PURPLE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
