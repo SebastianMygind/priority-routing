0a9ae682a2c4a7e4265c8f15fe0f7ee8470779cb
Sebastian Mygind <sebmygind@gmail.com>
2026-02-18 11:19:44 +0100

    remove unused

--- main.cpp
+++ main.cpp
@@ -6,20 +6,18 @@ int main() {
     std::println("Hello World!");
 
     auto window = Window("Routing Simulation");
-    auto window_scale = GetWindowScaleDPI();
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
 
     InitWindow(window.width, window.height, window.title.c_str());
 
-    window.width = GetRenderWidth();
-    window.height = GetRenderHeight();
-
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
+
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+
         // Update
         if (IsWindowResized()) {
 
@@ -33,11 +31,11 @@ int main() {
 
         ClearBackground(RAYWHITE);
 
-        DrawText("Congrats! You created your first window!", 190, 200, 20, DARKGRAY);
+        // DrawText("Congrats! You created your first window!", 190, 200, 20, DARKGRAY);
 
-        DrawLineEx(Vector2 {20, 40 }, Vector2 {400, 30}, 7., DARKBLUE);
+        // DrawLineEx(Vector2 {20, 40 }, Vector2 {400, 30}, 7., DARKBLUE);
 
-        DrawCircle(window.width, window.height, 30, PURPLE);
+        DrawCircle(window.width/2, window.height/2, 30, PURPLE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
