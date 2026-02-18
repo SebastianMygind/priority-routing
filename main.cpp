9444364ee80a2ee7f10b47fd926bbf6fc9882896
Kristiyan <2003kristiyan@gmail.com>
2026-02-18 10:42:20 +0000

    Grid and camera

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <print>
 #include "raylib.h"
 #include "Window.h"
+#include "rlgl.h"
+#include "raymath.h"
 
 int main() {
     std::println("Hello World!");
@@ -9,6 +11,10 @@ int main() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT | FLAG_WINDOW_HIGHDPI);
 
     InitWindow(window.width, window.height, window.title.c_str());
+    SetTargetFPS(120);
+
+    Camera2D camera = {0};
+    camera.zoom = 1.0f;
 
     SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -24,17 +30,53 @@ int main() {
             window.height = GetScreenHeight();
         }
 
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f/camera.zoom);
+            camera.target = Vector2Add(camera.target, delta);
+        }
+
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0)
+        {
+            // Get the world point that is under the mouse
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+            // Set the offset to where the mouse is
+            camera.offset = GetMousePosition();
+
+            // Set the target to match, so that the camera maps the world space point
+            // under the cursor to the screen space point under the cursor at any zoom
+            camera.target = mouseWorldPos;
+
+            // Zoom increment
+            // Uses log scaling to provide consistent zoom speed
+            float scale = 0.2f*wheel;
+            camera.zoom = Clamp(expf(logf(camera.zoom)+scale), 0.125f, 64.0f);
+            std::println("{}", camera.zoom);
+        }
+
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        // DrawText("Congrats! You created your first window!", 190, 200, 20, DARKGRAY);
+        BeginMode2D(camera);
+                // Draw the 3d grid, rotated 90 degrees and centered around 0,0
+                // just so we have something in the XY plane
+                rlPushMatrix();
+                    rlTranslatef(0, 25*50, 0);
+                    rlRotatef(90, 1, 0, 0);
+                    DrawGrid(100, 50);
+                rlPopMatrix();
 
-        // DrawLineEx(Vector2 {20, 40 }, Vector2 {400, 30}, 7., DARKBLUE);
+                // Draw a reference circle
+                DrawCircle(window.width/2, window.height/2, 50, MAROON);
+            EndMode2D();
 
-        DrawCircle(window.width/2, window.height/2, 30, PURPLE);
+        DrawCircle(window.width, window.height/2, 100, PURPLE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
