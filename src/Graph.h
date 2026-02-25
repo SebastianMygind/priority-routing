8b3ec3ee853fbbf540421c81b6bd615eb9de6c6b
DandingB <8073687+DandingB@users.noreply.github.com>
2026-02-25 11:41:01 +0100

    Refactoring and mercator (#3)

--- src/Graph.h
+++ src/Graph.h
@@ -4,12 +4,14 @@
 #include <cstdint>
 #include <string>
 
+#include "raylib.h"
+
 struct Node 
 {
-    float x, y;
+    double lat, lon;
 };
 
-struct Edge 
+struct Way 
 {
     std::vector<uint64_t> nodeRefs;
     std::unordered_map<std::string, std::string> tags;
@@ -20,7 +22,7 @@ class Graph
 {
 public:
     std::unordered_map<uint64_t, Node> nodes;
-    std::vector<Edge> edges;
+    std::vector<Way> ways;
 
     std::vector<uint64_t> selected_path;
     uint64_t selected_node_a;
@@ -28,5 +30,8 @@ public:
 
     Graph();
 
-    void DrawGraph();
+    void DrawGraph(Camera2D camera, float screenWidth, float screenHeight);
 };
+
+
+inline Vector2 MercatorProjection(double lat, double lon, float screenHeight, float screenWidth);
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -9,7 +9,7 @@ bool ParseOSM(std::string path, Graph& out_graph)
     tinyxml2::XMLDocument doc;
 
     out_graph.nodes.clear();
-    out_graph.edges.clear();
+    out_graph.ways.clear();
 
     if (doc.LoadFile(path.c_str()) != tinyxml2::XML_SUCCESS)
     {
@@ -20,43 +20,42 @@ bool ParseOSM(std::string path, Graph& out_graph)
     tinyxml2::XMLNode* root = doc.FirstChildElement("osm");
 
     {
-    tinyxml2::XMLElement* element = root->FirstChildElement("way");
+        tinyxml2::XMLElement* element = root->FirstChildElement("way");
 
-    while (element)
-    {
-        Edge way = {};
-
-        tinyxml2::XMLElement* child = element->FirstChildElement("nd");
-        while (child)
+        while (element)
         {
-            uint64_t ref = std::stoull(child->Attribute("ref"));
-            way.nodeRefs.push_back(ref);
-            child = child->NextSiblingElement("nd");
+            Way way = {};
+
+            tinyxml2::XMLElement* child = element->FirstChildElement("nd");
+            while (child)
+            {
+                uint64_t ref = std::stoull(child->Attribute("ref"));
+                way.nodeRefs.push_back(ref);
+                child = child->NextSiblingElement("nd");
+            }
+
+            tinyxml2::XMLElement* childTag = element->FirstChildElement("tag");
+            while (childTag)
+            {
+                way.tags.insert({ 
+                    std::string(childTag->Attribute("k")), 
+                    std::string(childTag->Attribute("v")) 
+                });
+
+                childTag = childTag->NextSiblingElement("tag");
+            }
+
+            if (auto tag = way.tags.find("highway"); tag != way.tags.end())
+            {
+                out_graph.ways.push_back(way);
+            }
+            
+            //tinyxml2::XMLElement* tag = element->FirstChildElement("tag");
+            //if (tag)
+            //    way.type = std::string(tag->Attribute("k"));
+
+            element = element->NextSiblingElement("way");
         }
-
-        tinyxml2::XMLElement* childTag = element->FirstChildElement("tag");
-        while (childTag)
-        {
-            way.tags.insert({ 
-                std::string(childTag->Attribute("k")), 
-                std::string(childTag->Attribute("v")) 
-            });
-
-            childTag = childTag->NextSiblingElement("tag");
-        }
-
-        if (auto tag = way.tags.find("highway"); tag != way.tags.end())
-        {
-            out_graph.edges.push_back(way);
-        }
-        
-        //tinyxml2::XMLElement* tag = element->FirstChildElement("tag");
-        //if (tag)
-        //    way.type = std::string(tag->Attribute("k"));
-
-        
-
-        element = element->NextSiblingElement("way");
     }
 
     {
@@ -64,28 +63,19 @@ bool ParseOSM(std::string path, Graph& out_graph)
 
         while (element)
         {
-            const char* asd = element->Attribute("id");
             uint64_t id = std::stoull(element->Attribute("id"));
             double lat = std::stod(element->Attribute("lat"));
             double lon = std::stod(element->Attribute("lon"));
 
-            lat -= 55.6539977;
-            lon -= 12.5422305;
-
-            lat *= 700000.0;
-            lon *= 500000.0;
-
-            for (Edge& edge : out_graph.edges)
+            for (Way& edge : out_graph.ways)
             {
                 if (std::find(edge.nodeRefs.begin(), edge.nodeRefs.end(), id) != edge.nodeRefs.end())
-                    out_graph.nodes.insert({ id, Node{(float)lon, (float)-lat} });
+                    out_graph.nodes.insert({ id, Node{lat, lon} });
             }
 
             element = element->NextSiblingElement("node");
         }
     }
-
-   
-    }
+    
     return true;
 }
