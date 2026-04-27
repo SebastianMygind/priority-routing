34771585d02d860b5889dc58fb6ac5a4e60d5fae
Sebastian Mygind <sebmygind@gmail.com>
2026-04-27 10:07:15 +0200

    Remove unused files

--- src/osm/graph.h
+++ src/osm/graph.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "raylib.h"
-#include "../osm_types.h"
-#include "../attribute_utils.h"
-#include "../caching.h"
+#include "osm_types.h"
 
 #include <vector>
 #include <unordered_map>
@@ -94,8 +92,6 @@ private:
     std::unordered_map<OSMNodeID, OSMNodeID> adj_list_prev;
 
     std::vector<OSMNodeID> getNodesWithTourism() const;
-    file_format_t GetNodeAttributes();
-    file_format_t GenerateNodeAttributes(attr_map_t attrInfo);
 
     // Nodes that is not a part of a way
     std::vector<OSMNodeID> places;
